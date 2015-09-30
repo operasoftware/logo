7ea272879c554457002b91ed74cba7e6d256a173
Vadim Makeev <pepelsbey@gmail.com>
2015-09-30 15:54:18 +0200

    Now with showcase page

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,48 @@
-var gulp = require('gulp'),
-	svgmin = require('gulp-svgmin');
+var autoprefixer = require('gulp-autoprefixer'),
+	beml = require('gulp-beml'),
+	minify = require('gulp-minify-css'),
+	gulp = require('gulp'),
+	htmlmin = require('gulp-htmlmin'),
+	sass = require('gulp-sass'),
+	svgmin = require('gulp-svgmin'),
+	sync = require('browser-sync').create();
 
-gulp.task('default', function () {
+gulp.task('default', ['svg', 'html', 'styles'], function () {
+	sync.init({
+		notify: false,
+		server: {
+			baseDir: '.'
+		}
+	});
+
+	gulp.watch('src/screen.scss', ['styles']);
+	gulp.watch('src/index.html', ['html']).on('change', sync.reload);
+});
+
+gulp.task('svg', function () {
 	return gulp.src('src/**/*.svg')
 		.pipe(svgmin())
 		.pipe(gulp.dest('dest'));
+});
+
+gulp.task('html', function() {
+	return gulp.src('src/index.html')
+		.pipe(beml({
+			elemPrefix: '__',
+			modPrefix: '--' }))
+		.pipe(htmlmin({
+			removeComments: true,
+			collapseWhitespace: true
+		}))
+		.pipe(gulp.dest('.'))
+		.pipe(sync.stream());
+});
+
+gulp.task('styles', function () {
+	return gulp.src('src/screen.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(minify())
+		.pipe(gulp.dest('.'))
+		.pipe(sync.stream());
 });
